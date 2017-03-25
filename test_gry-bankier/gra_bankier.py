9d6af3023f7c6246f04f194db6e8e6707b669f62
elvis <soownic@gmail.com>
2017-03-25 20:10:09 +0100

    Create gra_bankier.py

---
+++ test_gry-bankier/gra_bankier.py
@@ -0,0 +1,12 @@
+#coding=UTF-8
+#12.02.2017
+#GRA BANKIER polega na uzbieraniu jak najwięcej pieniędzy...
+#...w przeciągu ustalonego czasu
+
+from gra_bankier_modules import *
+
+graIntro()
+rysMainMenu()
+# wybMainMenu()
+zapDoPliku("test.test","test1\ntest2",1)
+# print("bankier gra ver %s" % gra_wersja)
